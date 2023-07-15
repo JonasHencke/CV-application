01431eb808e4517e4f3c7f715343396b5b1da029
jonas-1997 <jvhencke@googlemail.com>
2023-07-15 20:36:06 +0200

    add style to image upload button

--- src/CV.js
+++ src/CV.js
@@ -1,6 +1,6 @@
-import telephone from "./telephone-receiver-material-svgrepo-com.svg"
-import location from "./location-pin-svgrepo-com.svg"
-import email from "./email-svgrepo-com.svg"
+import telephone from "./SVG/telephone-receiver-material-svgrepo-com.svg"
+import location from "./SVG/location-pin-svgrepo-com.svg"
+import email from "./SVG/email-svgrepo-com.svg"
 import defaultProfile from "./Blank_portrait,_male_(rectangular).png"
 export default function CV(props) {
 
--- src/Form.js
+++ src/Form.js
@@ -1,17 +1,18 @@
-import trashbin from "./trash-full-svgrepo-com.svg"
+import trashbin from "./SVG/trash-full-svgrepo-com.svg"
+import uploadSVG from "./SVG/download-svgrepo-com.svg"
 import { nanoid } from 'nanoid'
 
 export default function Form(props) {
 
     const mapSkills = props.skills.map(
-        skill => <div className="Form--skillBundle"><input type="text" placeholder="Fähigkeit" name={skill.name} value={skill.value} key={skill.key} onChange={props.method2}/><div className="Form--deleteSkillBtn" onClick={props.removeSkill(skill.key)}><img className="Form--icon" src={trashbin}/></div></div> )
+        skill => <div className="Form--skillBundle"><input className="form--input" type="text" placeholder="Fähigkeit" name={skill.name} value={skill.value} key={skill.key} onChange={props.method2}/><div className="Form--deleteSkillBtn" onClick={props.removeSkill(skill.key)}><img className="Form--icon" src={trashbin}/></div></div> )
     
     const mapWorkExperience = props.workExperience.map(
         workExperience =>
         <div key={workExperience.key} className="form--workExperience">
-            <input type="text" keyw={workExperience.key} placeholder="Zeitraum" name="timeframe" value={workExperience.timeframe} onChange={props.method3}></input>
-            <input type="text" keyw={workExperience.key} placeholder="Titel" name="jobTitle" value={workExperience.jobTitle} onChange={props.method3}></input>
-            <input type="text" keyw={workExperience.key} placeholder="Unternehmen" name="company" value={workExperience.company}  onChange={props.method3}></input>
+            <input className="form--input" type="text" keyw={workExperience.key} placeholder="Zeitraum" name="timeframe" value={workExperience.timeframe} onChange={props.method3}></input>
+            <input className="form--input" type="text" keyw={workExperience.key} placeholder="Titel" name="jobTitle" value={workExperience.jobTitle} onChange={props.method3}></input>
+            <input className="form--input" type="text" keyw={workExperience.key} placeholder="Unternehmen" name="company" value={workExperience.company}  onChange={props.method3}></input>
             <textarea type="text" keyw={workExperience.key} placeholder="Beschreibung" name="description" value={workExperience.description}  onChange={props.method3}></textarea>
         </div>
     )
@@ -19,9 +20,9 @@ export default function Form(props) {
     const mapEducation = props.education.map(
         education =>
         <div key={education.key} className="form--workExperience">
-            <input type="text" keyw={education.key} placeholder="Zeitraum" name="timeframe" value={education.timeframe} onChange={props.method4}></input>
-            <input type="text" keyw={education.key} placeholder="Titel" name="jobTitle" value={education.jobTitle} onChange={props.method4}></input>
-            <input type="text" keyw={education.key} placeholder="Unternehmen" name="company" value={education.company}  onChange={props.method4}></input>
+            <input className="form--input" type="text" keyw={education.key} placeholder="Zeitraum" name="timeframe" value={education.timeframe} onChange={props.method4}></input>
+            <input className="form--input" type="text" keyw={education.key} placeholder="Titel" name="jobTitle" value={education.jobTitle} onChange={props.method4}></input>
+            <input className="form--input" type="text" keyw={education.key} placeholder="Unternehmen" name="company" value={education.company}  onChange={props.method4}></input>
             <textarea type="text" keyw={education.key} placeholder="Beschreibung" name="description" value={education.description}  onChange={props.method4}></textarea>
         </div>
     )
@@ -34,17 +35,19 @@ export default function Form(props) {
             <div className="form">
                 <h1>Persönliches</h1>
                 <div className="input--container">
-                    <input type="text" name="firstName" value={props.value.firstName} placeholder="Vorname" onChange={props.method}/>
-                    <input type="text" name="lastName" value={props.value.lastName} placeholder="Nachname" onChange={props.method}/>
+                    <input className="form--input" type="text" name="firstName" value={props.value.firstName} placeholder="Vorname" onChange={props.method}/>
+                    <input className="form--input" type="text" name="lastName" value={props.value.lastName} placeholder="Nachname" onChange={props.method}/>
                 </div>
-                <input type="text" name="jobTitle" value={props.value.jobTitle} placeholder="Berufstitel" onChange={props.method}/>
-                <input type="text" name="address" value={props.value.address} placeholder="Adresse" onChange={props.method}/>
-                <input type="text" name="website" value={props.value.website} placeholder="Webseite" onChange={props.method}/>
-                <input type="text" name="email" value={props.value.email} placeholder="E-Mail" onChange={props.method}/>
-                <input type="text" name="phone" value={props.value.phone} placeholder="Telefonnummer" onChange={props.method}/>
-                <input type="file" id="file" onChange={(e) => props.setPhoto(URL.createObjectURL(e.target.files[0]))} accept="image/jpeg, image/png, image/jpg" placeholder="Profilbild"/>
+                <input className="form--input" type="text" name="jobTitle" value={props.value.jobTitle} placeholder="Berufstitel" onChange={props.method}/>
+                <input className="form--input" type="text" name="address" value={props.value.address} placeholder="Adresse" onChange={props.method}/>
+                <input className="form--input" type="text" name="website" value={props.value.website} placeholder="Webseite" onChange={props.method}/>
+                <input className="form--input" type="text" name="email" value={props.value.email} placeholder="E-Mail" onChange={props.method}/>
+                <input className="form--input" type="text" name="phone" value={props.value.phone} placeholder="Telefonnummer" onChange={props.method}/>
                 <textarea type="text" name="Beschreibung" value={props.value.Beschreibung} placeholder="Über dich" onChange={props.method}/>
+                <input className="displayNone" type="file" id="file" onChange={(e) => props.setPhoto(URL.createObjectURL(e.target.files[0]))} accept="image/jpeg, image/png, image/jpg"/>
+                <label className="uploadBtn" for="file"><img className="Form--icon" src={uploadSVG}></img>Upload Photo</label>
             </div>
+
             <div className="form">
                 <h1>Skills</h1>
                 {mapSkills}
